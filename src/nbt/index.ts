4faeae22ada3207768e6f5d77d9f3f410e5b7f56
PMK744 <paytonmk744@outlook.com>
2024-01-28 13:33:19 -0600

    Added MappedBlockStates

--- src/nbt/index.ts
+++ src/nbt/index.ts
@@ -1,8 +1,10 @@
 import { Buffer } from 'node:buffer';
 import BiomeDefinitionList from './biome_definition_list.json';
 import CanonicalBlockStates from './canonical_block_states.json';
+import MappedBlockStates from './mapped_block_states.json';
 
 const CANONICAL_BLOCK_STATES = Buffer.from(CanonicalBlockStates as number[]);
 const BIOME_DEFINITION_LIST = Buffer.from(BiomeDefinitionList as number[]);
+const MAPPED_BLOCK_STATES = Buffer.from(MappedBlockStates as number[]);
 
-export { CANONICAL_BLOCK_STATES, BIOME_DEFINITION_LIST };
+export { CANONICAL_BLOCK_STATES, BIOME_DEFINITION_LIST, MAPPED_BLOCK_STATES };
